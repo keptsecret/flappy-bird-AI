3b30fec3515139b990d9970c1d3eb4fb401892a5
keptsecret <sorchon@gmail.com>
2022-05-09 16:53:13 +0700

    some more neat modules

---
+++ flappy-bird-ai/neat/population.py
@@ -0,0 +1,182 @@
+from __future__ import annotations
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..player import Player
+    from .connection import ConnectionHistory
+
+from .species import Species
+
+class Population():
+    def __init__(self, pop_size : int) -> None:
+        self.players : List[Player] = []
+        self.best_player : Player = None
+        self.best_score = 0
+        self.global_best_score = 0
+        self.gen = 1
+        self.innovation_history : List[ConnectionHistory] = []
+        self.gen_players : List[Player] = []
+        self.species : List[Species] = []
+
+        self.is_mass_extinction = False
+        self.is_new_stage = False
+
+        self.gens_since_new_world = 0
+
+        for _ in range(pop_size):
+            self.players.append(Player())
+            self.players[-1].brain.mutate(self.innovation_history)
+            self.players[-1].brain.generate_network()
+
+    def get_current_best(self) -> Player:
+        for p in self.players:
+            if p.is_alive:
+                return p
+        return self.players[0]
+
+    def update_alive(self) -> None:
+        for p in self.players:
+            if p.is_alive:
+                # TODO: might add multiple steps in one update
+                p.look()
+                p.think()
+                p.update()
+
+            # TODO: add selective draw to show only some of the players
+            p.draw()
+
+            if p.score > self.global_best_score:
+                self.global_best_score = p.score
+
+    def done(self) -> bool:
+        """
+        Returns if players are all dead
+        """
+        for p in self.players:
+            if p.is_alive:
+                return False
+        return True
+
+    def select_best_player(self) -> None:
+        temp_best = self.species[0].players[0]
+        temp_best.gen = self.gen
+
+        if temp_best.score > self.best_score:
+            self.gen_players.append(temp_best.clone())
+            print(f'Old best: {self.best_score}\tNew best: {temp_best.score}')
+            self.best_score = temp_best.score
+            self.best_player = temp_best.clone()
+
+    def natural_selection(self) -> None:
+        """
+        Make new generation when all players are dead
+        """
+        prev_best = self.players[0]
+        
+        self.speciate()
+        self.calculate_fitness()
+        self.sort_species()
+        if self.is_mass_extinction:
+            self.mass_extinction()
+            self.is_mass_extinction = False
+
+        self.cull_species()
+        self.select_best_player()
+        self.kill_species()
+
+        avg_sum = self.get_avg_fitness_sum()
+        children : List[Player] = []
+        for s in self.species:
+            children.append(s.champ.clone())
+
+            num_children = s.avg_fitness // avg_sum * len(self.players)
+
+            for _ in range(num_children - 1):
+                children.append(s.make_child(self.innovation_history))
+
+        # if not enough, then get clones from best player and best species
+        if len(children) < len(self.players):
+            children.append(prev_best.clone())
+
+        while len(children) < len(self.players):
+            children.append(self.species[0].make_child(self.innovation_history))
+
+        self.players = children.copy()
+        self.gen += 1
+        for p in self.players:
+            p.brain.generate_network()
+
+    def speciate(self) -> None:
+        for s in self.species:
+            s.players = []
+
+        for p in self.players:
+            species_found = False
+
+            # look for similar species and add to it
+            for s in self.species:
+                if s.is_same_species(p.brain):
+                    s.add_to_species(p)
+                    species_found = True
+                    break
+            
+            # if no similar species found, then make new one
+            if not species_found:
+                self.species.append(Species(p))
+
+    def calculate_fitness(self):
+        for p in self.players:
+            p.calculate_fitness()
+
+    def sort_species(self) -> None:
+        for s in self.species:
+            s.sort_species()
+
+        temp = []
+
+        while len(self.species) > 0:
+            max_val = 0
+            max_idx = 0
+            
+            for i, s2 in enumerate(self.species):
+                if s2.best_fitness > max_val:
+                    max_val = s2.best_fitness
+                    max_idx = i
+            
+            temp.append(self.species[max_idx])
+            del self.species[max_idx]
+
+        self.species = temp.copy()
+
+    def kill_species(self) -> None:
+        """
+        Kills species that are stale (haven't improved in 15 generations) or bad (won't give a child)
+        """
+        i = 0
+        avg_sum = self.get_avg_fitness_sum()
+        while i < len(self.species):
+            if (self.species[i].staleness >= 15) or (self.species[i].avg_fitness / avg_sum * len(self.players) < 1):
+                del self.species[i]
+            else:
+                i += 1
+
+    def cull_species(self) -> None:
+        """
+        Kill bottom half of each species
+        """
+        for s in self.species:
+            s.cull()
+            s.fitness_sharing()
+            s.set_avg_fitness()
+
+    def mass_extinction(self) -> None:
+        """
+        Keep only top 5 species
+        """
+        self.species = self.species[:5]
+
+    def get_avg_fitness_sum(self):
+            avg_sum = 0
+            for s in self.species:
+                avg_sum += s.avg_fitness
+            return avg_sum
---
+++ flappy-bird-ai/neat/species.py
@@ -0,0 +1,164 @@
+from __future__ import annotations
+from typing import List, TYPE_CHECKING
+import random
+
+if TYPE_CHECKING:
+    from ..player import Player
+    from .genome import Genome
+    from .connection import ConnectionHistory
+
+class Species():
+    def __init__(self, player : Player) -> None:
+        self.players : List[Player] = []
+        self.best_fitness = 0
+        self.champ = None
+        self.avg_fitness = 0
+        self.staleness = 0
+        self.rep = None
+
+        self.excess_cof = 1
+        self.weight_diff_cof = 0.5
+        self.compat_thresh = 3
+
+        if player != None:
+            self.players.append(player)
+            self.best_fitness = player.fitness
+            self.rep = player.brain.clone()
+            self.champ = player.clone()         # might not use, if not implement replay
+
+    def is_same_species(self, g : Genome) -> bool:
+        """
+        Returns whether this and param genome is in same species i.e., similar genes
+        """
+        excess_and_disjoint = self.get_excess_disjoint(g, self.rep)
+        avg_weight_diff = self.avg_weight_diff(g, self.rep)
+
+        normalizer = max(len(g.genes), 1)
+        compat = (self.excess_cof * excess_and_disjoint / normalizer) + (self.weight_diff_cof * avg_weight_diff)    # compatibility formula
+        return compat < self.compat_thresh
+
+    def get_excess_disjoint(self, brain1 : Genome, brain2 : Genome) -> float:
+        """
+        Returns number of excess and disjoint genes (those that don't match)
+        """
+        matching = 0.0
+        for conn1 in brain1.genes:
+            for conn2 in brain2.genes:
+                if conn1.innovation_num == conn2.innovation_num:
+                    matching += 1
+                    break
+        
+        return len(brain1.genes) + len(brain2.genes) - 2 * matching
+
+    def avg_weight_diff(self, brain1 : Genome, brain2 : Genome) -> float:
+        """
+        Returns average weight difference between matching genes in genomes
+        """
+        if len(brain1.genes) == 0 or len(brain2.genes):
+            return 0
+
+        matching = 0.0
+        total_diff = 0.0
+        for conn1 in brain1.genes:
+            for conn2 in brain2.genes:
+                if conn1.innovation_num == conn2.innovation_num:
+                    matching += 1
+                    total_diff += abs(conn1.weight - conn2.weight)
+                    break
+
+        if matching == 0:
+            return 100
+
+        return total_diff / matching
+
+    def add_to_species(self, player : Player) -> None:
+        self.players.append(player)
+
+    def sort_species(self) -> None:
+        """
+        Sorts players by fitness, in decreasing order
+        """
+        temp = []
+
+        while len(self.players) > 0:
+            max_val = 0
+            max_idx = 0
+            
+            for i, p2 in enumerate(self.players):
+                if p2.fitness > max_val:
+                    max_val = p2.fitness
+                    max_idx = i
+            
+            temp.append(self.players[max_idx])
+            del self.players[max_idx]
+
+        self.players = temp.copy()
+        if len(self.players) == 0:
+            self.staleness = 200
+            return
+        
+        if self.players[0].fitness > self.best_fitness:
+            self.staleness = 0
+            self.best_fitness = self.players[0].fitness
+            self.rep = self.players[0].brain.clone()
+            self.champ = self.players[0].clone()
+        else:
+            self.staleness += 1
+
+    def set_avg_fitness(self) -> None:
+        total = 0
+        for p in self.players:
+            total += p.fitness
+        
+        self.avg_fitness = total / len(self.players)
+
+    def make_child(self, innovation_hist : List[ConnectionHistory]) -> Player:
+        """
+        Makes a child from all players in this species
+        """
+
+        if (random.random() < 0.25):
+            baby = self.select_player().clone()
+        else:
+            p1 = self.select_player()
+            p2 = self.select_player()
+
+            if p1.fitness < p2.fitness:
+                baby = p2.crossover(p1)
+            else:
+                baby = p1.crossover(p2)
+        
+        baby.brain.mutate(innovation_hist)
+        return baby
+
+    def select_player(self) -> Player:
+        """
+        Returns a player based on fitness semi-randomly
+        """
+        fitness_sum = 0
+        for p in self.players:
+            fitness_sum += p.fitness
+        
+        rand = random.random() * fitness_sum
+        running = 0
+        for p in self.players:
+            running += p.fitness
+            if running > rand:
+                return p
+        
+        return self.players[0]
+
+    def cull(self) -> None:
+        """
+        Removes bottom half of players
+        """
+        p_len = len(self.players)
+        if p_len > 2:
+            self.players = self.players[:p_len // 2]
+
+    def fitness_sharing(self) -> None:
+        """
+        Protect unique players by scaling down players' fitnesses with number of players in its species
+        """
+        for p in self.players:
+            p.fitness /= len(self.players)
