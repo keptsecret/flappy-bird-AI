e7cd5956aea0571534fa778f93e806b8678a1862
keptsecret <sorchon@gmail.com>
2022-05-09 17:21:45 +0700

    first implemented ai game loop

--- flappy-bird-ai/main.py
+++ flappy-bird-ai/main.py
@@ -4,12 +4,17 @@ import pygame
 
 from player import Player
 from pipe import PipePair
+from population import Population
 
 bird_yellow = (250, 239, 32)
 pipe_green = (105, 214, 21)
 sky_blue = (52, 213, 235)
 
-def game_loop(screen, size, player, pipe_pairs):
+window_size = (500, 720)
+font_size = 60
+debug = False
+
+def game_loop(screen, size, font, player, pipe_pairs):
     prev_time = time()
 
     while True:
@@ -23,43 +28,88 @@ def game_loop(screen, size, player, pipe_pairs):
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    player.handle_input(curr_time)
+                    player.flap(curr_time)
         
         curr_time = time()
         surf = pygame.Surface(size)
         surf.fill(sky_blue)
 
         delta_time = curr_time - prev_time
-        pipe_list = []
         for pair in pipe_pairs:
             if pair.active:
                 if pair.top_pipe.position[0] < size[0] / 2 - 50:
                     pipe_pairs[1].start()
                 pair.update(delta_time)
-                pipe_list += pair.get_pipes()
-        player.update(2, delta_time, pipe_list)
+        player.update(delta_time)
 
         for pair in pipe_pairs:
             if pair.active:
                 pair.draw(surf)
         player.draw(surf)
 
+        text_surf = font.render(f'{player.score}', True, (255,255,255))
+        if debug:
+            debug_surf = font.render(f'{player.velocity}', True, (0,0,0))
+
         screen.blit(surf, (0,0))
+        screen.blit(text_surf, (size[0]/2 - font_size/2, 0))
+        if debug:
+            screen.blit(debug_surf, (0, 0))
+        pygame.display.flip()
+        prev_time = curr_time
+
+
+def game_loop_ai(screen, size, font, population, pipe_pairs):
+    prev_time = time()
+
+    while True:
+        for event in pygame.event.get():
+            curr_time = time()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        curr_time = time()
+        surf = pygame.Surface(size)
+        surf.fill(sky_blue)
+
+        delta_time = curr_time - prev_time
+        for pair in pipe_pairs:
+            if pair.active:
+                if pair.top_pipe.position[0] < size[0] / 2 - 50:
+                    pipe_pairs[1].start()
+                pair.update(delta_time)
+
+        for pair in pipe_pairs:
+            if pair.active:
+                pair.draw(surf)
+
+        if not population.done():
+            population.update_alive(surf, delta_time)
+        else:
+            population.natural_selection()
+
+        text_surf = font.render(f'{population.global_best_score}', True, (255,255,255))
+
+        screen.blit(surf, (0,0))
+        screen.blit(text_surf, (size[0]/2 - font_size/2, 0))
         pygame.display.flip()
         prev_time = curr_time
 
 
 def main():
     pygame.init()
+    pygame.font.init()
 
-    window_size = (500, 720)
+    font = pygame.font.SysFont('Arial', font_size, True)
 
     screen = pygame.display.set_mode(window_size)
-    player = Player()
     pipe_pairs = [PipePair(500), PipePair(500)]
     pipe_pairs[0].start()
+    # player = Player(pipes=pipe_pairs)
+    population = Population(100)
 
-    game_loop(screen, window_size, player, pipe_pairs)
+    # game_loop(screen, window_size, font, player, pipe_pairs)
+    game_loop_ai(screen, window_size, font, population, pipe_pairs)
 
     pygame.quit()
     sys.exit()
--- flappy-bird-ai/player.py
+++ flappy-bird-ai/player.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
+from typing import List
 import pygame
 
 from neat.genome import Genome
 
 class Player():
-    def __init__(self, pos=(200, 720 / 3), size=30.0, color=(250, 239, 32)) -> None:
+
+    pipe_pairs = None
+    gravity = 3
+
+    def __init__(self, pos=(500 / 5 * 2, 720 / 3), size=30.0, color=(250, 239, 32), pipes=None) -> None:
         self.position = pos
         self.size = size
         self.color = color
@@ -12,6 +17,10 @@ class Player():
 
         self.last_pipe = 1
         self.last_input_time = None
+        self.max_speed = 0.8
+        
+        if Player.pipe_pairs is None:
+            Player.pipe_pairs = pipes
 
         # NEAT stuff
         self.fitness = 0
@@ -28,16 +37,16 @@ class Player():
         self.genome_outputs = 1
         self.brain = Genome(self.genome_inputs, self.genome_outputs)
 
-    def handle_input(self, delta_time) -> None:
+    def flap(self, delta_time) -> None:
         if self.last_input_time == None or delta_time - self.last_input_time >= 0.15:
-            self.velocity = -0.7
+            self.velocity = -self.max_speed
             self.last_input_time = delta_time
 
-    def update(self, gravity, delta_time, pipe_list) -> None:
+    def update(self, delta_time) -> None:
         def clamp(num, min_value, max_value):
             return max(min(num, max_value), min_value)
 
-        self.velocity += gravity * delta_time
+        self.velocity = clamp(self.velocity + Player.gravity * delta_time, -self.max_speed, self.max_speed)
         pos_y = clamp(self.position[1] + self.velocity, 0+self.size, 720-self.size)
         self.position = (self.position[0], pos_y)
 
@@ -46,6 +55,9 @@ class Player():
             self.is_alive = False
 
         # handle collisions with pipe
+        pipe_list = []
+        for pp in self.pipe_pairs:
+            pipe_list += pp.get_pipes()
         for i, pipe in enumerate(pipe_list):
             collide_x = self.position[0] + self.size > pipe.position[0] and self.position[0] - self.size < pipe.position[0] + pipe.width
             collide_y = self.position[1] + self.size > pipe.position[1] and self.position[1] - self.size < pipe.position[1] + pipe.height
@@ -58,16 +70,55 @@ class Player():
             if dist_past > 0 and i // 2 != self.last_pipe:
                 self.score += 1
                 self.last_pipe = i // 2
-                print(f'Score: {self.score}')
 
     def draw(self, surface) -> None:
         pygame.draw.circle(surface, self.color, self.position, self.size)
 
+    # ------------------------------------------------------------------------
+    # functions for NEAT
+
+    def look(self):
+        def normalize(value, old_range, new_range) -> float:
+            value = (value - old_range[0]) / (old_range[0] + old_range[1])
+            value = value / (new_range[0] + new_range[1]) + new_range[0]
+            return value
+
+        self.vision = [0, 0, 0, 0]
+        self.vision[0] = normalize(self.velocity, (-0.75, 0.75), (-1, 1))
+
+        closest_pipe = self.pipe_pairs[1 - self.last_pipe]
+
+        bottom_pipe = closest_pipe.bottom_pipe
+        # distance to closest pipe
+        self.vision[1] = normalize(bottom_pipe.position[0] - self.position[0], (0, 720), (0, 1))
+        # distance to top of bottom pipe
+        self.vision[2] = normalize(bottom_pipe.position[1] - self.position[1], (0, 720), (0, 1))
+        # distance to bottom of top pipe
+        top_pipe = closest_pipe.top_pipe
+        self.vision[3] = normalize(self.position[1] - top_pipe.position[1] + top_pipe.height, (0, 720), (0, 1))
+
+    def think(self):
+        self.decision = self.brain.feedforward(self.vision)
+        if self.decision[0] > 0.6:
+            self.flap()
+
+    def calculate_fitness(self) -> None:
+        self.fitness = 1 + pow(self.score, 2) + self.lifespan / 20.0
+
     def crossover(self, other_parent : Player) -> Player:
-        pass
+        child = Player()
+        child.brain = self.brain.crossover(other_parent.brain)
+        child.brain.generate_network()
+        return child
 
     def clone(self) -> Player:
         """
         TODO ?
         """
-        pass
+        clone = Player()
+        clone.brain = self.brain.clone()
+        clone.fitness = self.fitness
+        clone.brain.generate_network()
+        clone.gen = self.gen
+        clone.best_score = self.score
+        return clone
