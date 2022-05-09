a7202ffda9cef927a99355249d3174bba15d7732
keptsecret <sorchon@gmail.com>
2022-05-09 23:57:30 +0700

    reverted accidental changes

--- flappy-bird-ai/player.py
+++ flappy-bird-ai/player.py
@@ -23,9 +23,6 @@ class Player():
         if Player.pipe_pairs is None:
             Player.pipe_pairs = pipes
 
-        self.img = pygame.image.load(os.path.join("images", "duck.png"))
-        self.img = pygame.transform.scale(self.img, (radius*2, radius*2))
-
         # NEAT stuff
         self.fitness = 0
         self.vision = []        # input for NN
@@ -78,8 +75,7 @@ class Player():
         self.last_input_time += delta_time
 
     def draw(self, surface) -> None:
-        # pygame.draw.circle(surface, self.color, self.position, self.radius)
-        surface.blit(self.img, (self.position[0]-self.radius, self.position[1]-self.radius))
+        pygame.draw.circle(surface, self.color, self.position, self.radius)
 
     # ------------------------------------------------------------------------
     # functions for NEAT
